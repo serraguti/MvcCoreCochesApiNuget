6da32ec0ce799b8a2ae125cf99c362d7926e1fef
serraguti <pacoserranox@gmail.com>
2023-01-27 16:43:54 +0100

    Add project files.

---
+++ MvcCoreCochesApiNuget/Controllers/CochesController.cs
@@ -0,0 +1,16 @@
+﻿using JuernesCochesApiNuget;
+using JuernesCochesApiNuget.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreCochesApiNuget.Controllers
+{
+    public class CochesController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            CochesContext context = new CochesContext();
+            List<Coche> coches = await context.GetCochesAsync();
+            return View(coches);
+        }
+    }
+}
